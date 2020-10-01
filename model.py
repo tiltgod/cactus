68fe90f97c1b89a17d48f9e2a470b3a26c68d81e
Napasin Hongngern <napasin_h@Napasins-MacBook-Pro.local>
2020-10-01 20:56:59 +0700

    Seperate model from cactus.py

--- model.py
+++ model.py
@@ -5,6 +5,9 @@ from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Global
 from tensorflow.keras import optimizers, regularizers
 from tensorflow.keras.callbacks import Callback, EarlyStopping, LearningRateScheduler
 
+np.random.seed(12)
+tf.random.set_seed(12)
+
 model = Sequential()
 model.add(Conv2D(filters=32, kernel_size=(3,3), padding='same', activation='relu', input_shape=input_shape))
 model.add(MaxPooling2D(pool_size=(2,2)))
